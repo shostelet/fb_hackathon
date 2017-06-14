6c034747cc85956479b69068a892f09b2857bb06
shostelet <simon.hostelet@gmail.com>
2017-06-14 17:35:27 +0200

    Procfile and setup to make it run on Cloud Foundry. But still fails to load some modules...

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dummyproject.dummyproject.wsgi:application
--- dummyproject/dummyproject/wsgi.py
+++ dummyproject/dummyproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dummyproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dummyproject.dummyproject.settings")
 
 application = get_wsgi_application()
