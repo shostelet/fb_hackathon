c865f5b710af91f4393d1f9f32d0ab683e7477c1
Karl <karl.wnw@gmail.com>
2017-06-14 13:34:49 +0200

    Add Profile and Topic model

--- dummyproject/messenger_bot_auth/models.py
+++ dummyproject/messenger_bot_auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+
+    messenger_app_user_id = models.IntegerField()
+
+
+class Topic(models.Model):
+
+    name = models.CharField(max_length=255)
+    profile = models.ForeignKey(Profile, related_name="topics")
